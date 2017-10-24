a65a95f47620a4b4818845d554222a6b345fb098
Pascal Sigel <pascal.sigel@gmail.com>
2017-10-24 11:58:56 -0300

    SoftmaxLayer updated

--- ann/macro_layer/layer_structure/layers/SoftmaxLayer.py
+++ ann/macro_layer/layer_structure/layers/SoftmaxLayer.py
@@ -5,22 +5,19 @@ class SoftmaxLayer(object):
     def __init__(self):
         self.output = None
 
-    def get_tensor(self, instance, owner):
+    def get_tensor(self):
         if self.output is not None:
             return self.output
         else:
             raise (ValueError, "Softmax Layer not connected, output does not exists")
 
-    def get_input_amount(self):
+    @staticmethod
+    def get_input_amount():
         return NotImplemented
 
-    def connect_layer(self, prev_layer_input_amount, input_tensor):
+    def connect_layer(self, _, input_tensor):
         self.output = tf.nn.softmax(input_tensor)
 
     @staticmethod
     def get_variables():
         return None
-
-
-
-
