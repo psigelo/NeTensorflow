bc28222a79e56a92f1a7f4a3b4e8eddcd97cb4da
Pascal Sigel <pascal.sigel@gmail.com>
2017-10-24 16:36:47 -0300

    ANN:: write_graph_and_summaries method added

--- ann/ANN.py
+++ ann/ANN.py
@@ -1,12 +1,18 @@
+import datetime
+
+import os
 import tensorflow as tf
 
 
 class ANN(object):
-    def __init__(self, macro_layers=None, tf_session=None):
+    def __init__(self, macro_layers=None, tf_session=None, base_folder='.'):
         self.macro_layers = macro_layers
         self.tf_session = tf_session
         self.last_layer = None
         self.first_layer = None
+        self.train_writer = None
+        self.tf_summaries = None
+        self.base_folder = base_folder
 
     def connect_and_initialize(self):
         self.connect()
@@ -26,6 +32,10 @@ class ANN(object):
 
         self.last_layer = layers_refs[-1]
         self.first_layer = layers_refs[0]
+        time_stamp = '{:%Y%m%d%H%M%S}'.format(datetime.datetime.now())
+        self.train_writer = \
+            tf.summary.FileWriter(os.path.join(time_stamp, self.base_folder, './train'), self.tf_session.graph)
+        self.tf_summaries = tf.summary.merge_all()
 
     def initialize(self):
         self.tf_session.run(tf.global_variables_initializer())
@@ -34,7 +44,8 @@ class ANN(object):
         input_tensor = self.first_layer.get_input_tensor()
         output_tensor = self.last_layer.get_tensor()
         import numpy as np
-        result = None
-        for i in range(1000):
-            result = self.tf_session.run(output_tensor, {input_tensor: [np.random.uniform(0.0, 10.0, 3)]})
-        return result
+        return self.tf_session.run(output_tensor, {input_tensor: [np.random.uniform(0.0, 10.0, 3)]})
+
+    def write_graph_and_summaries(self, global_iteration=None):
+        summaries = self.tf_session.run(self.tf_summaries)
+        self.train_writer.add_summary(summaries, global_iteration)
