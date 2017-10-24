1eca647c956a5eee8b2f5ebc43b4d0bc15a29867
Pascal Sigel <pascal.sigel@gmail.com>
2017-10-24 11:58:34 -0300

    FullConnectedWithSoftmaxLayerand InputLayer updated

--- ann/macro_layer/layer_structure/layers/FullConnectedWithSoftmaxLayer.py
+++ ann/macro_layer/layer_structure/layers/FullConnectedWithSoftmaxLayer.py
@@ -2,13 +2,13 @@ import tensorflow as tf
 
 
 class FullConnectedWithSoftmaxLayer(object):
-    def __init__(self, inputs_amount=None, params=None):
+    def __init__(self, inputs_amount=None):
         self.inputs_amount = inputs_amount
         self.output = None
         self.__weights = None
         self.__bias = None
 
-    def get_tensor(self, instance, owner):
+    def get_tensor(self):
         if self.output is not None:
             return self.output
         else:
@@ -22,13 +22,11 @@ class FullConnectedWithSoftmaxLayer(object):
             if len(prev_layer_input_amount) > 1:
                 raise (NotImplemented, "case not implemented:: input_size is list larger than 1 item")
         elif isinstance(prev_layer_input_amount, int):
-            self.__weights = tf.Variable(tf.zeros([prev_layer_input_amount, self.inputs_amount]))
-            self.__bias = tf.Variable()
+            self.__weights = tf.Variable(tf.truncated_normal([prev_layer_input_amount, self.inputs_amount], stddev=0.1))
+            self.__bias = tf.Variable(tf.constant(0.1, shape=[self.inputs_amount]))
             self.output = tf.nn.softmax(tf.matmul(input_tensor, self.__weights) + self.__bias)
         else:
             raise (NotImplemented, "input size type not recognized")
 
     def get_variables(self):
         return [self.__weights, self.__bias]
-
-
--- ann/macro_layer/layer_structure/layers/InputLayer.py
+++ ann/macro_layer/layer_structure/layers/InputLayer.py
@@ -3,25 +3,33 @@ import tensorflow as tf
 
 class InputLayer(object):
     def __init__(self, inputs_amount):
-        self.inputs_amount = inputs_amount
-        with tf.name_scope('Input'):
-            self.inputs = tf.placeholder(tf.float32, inputs_amount)
+        if isinstance(inputs_amount, int):
+            self.inputs_amount = inputs_amount
 
-    def get_tensor(self, instance, owner):
+        elif isinstance(inputs_amount, list):
+            if inputs_amount[0] is None:
+                self.inputs_amount = inputs_amount[1]
+            else:
+                raise Exception('NotImplemented', 'No yet implemented input case')
+        else:
+            raise Exception('NotImplemented', 'No yet implemented input case')
+
+        with tf.name_scope('Input'):
+            self.inputs = tf.placeholder(tf.float32, [None, self.inputs_amount])
+
+    def get_tensor(self):
         return self.inputs
 
     def get_input_amount(self):
         return self.inputs_amount
 
-    def connect_layer(self, prev_layer_input_amount, input_tensor):
+    @staticmethod
+    def connect_layer(_):
         assert False, "Error:: Connecting process start from second layer"
 
-    def get_variables(self):
+    @staticmethod
+    def get_variables():
         return None
 
     def get_input_tensor(self):
         return self.inputs
-
-
-
-
