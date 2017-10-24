228287b2e36f3a29518a7f5adae61f0ec340ed0f
Pascal Sigel <pascal.sigel@gmail.com>
2017-10-24 11:51:22 -0300

    ANN actualizada

--- ann/ANN.py
+++ ann/ANN.py
@@ -1,3 +1,6 @@
+import tensorflow as tf
+
+
 class ANN(object):
     def __init__(self, macro_layers=None, tf_session=None):
         self.macro_layers = macro_layers
@@ -6,6 +9,10 @@ class ANN(object):
         self.first_layer = None
 
     def connect_and_initialize(self):
+        self.connect()
+        self.initialize()
+
+    def connect(self):
         layers_refs = list()
 
         # first get all layers
@@ -15,13 +22,19 @@ class ANN(object):
 
         # Connect all layers
         for it in range(1, len(layers_refs)):  # Starting from second layer
-            layers_refs[it].connect(layers_refs[it-1].get_input_amount(), layers_refs[it-1].get_tensor())
+            layers_refs[it].connect_layer(layers_refs[it - 1].get_input_amount(), layers_refs[it - 1].get_tensor())
 
         self.last_layer = layers_refs[-1]
-        self.first_layer = layers_refs[1]
+        self.first_layer = layers_refs[0]
+
+    def initialize(self):
+        self.tf_session.run(tf.global_variables_initializer())
 
     def run(self):
         input_tensor = self.first_layer.get_input_tensor()
         output_tensor = self.last_layer.get_tensor()
-        import random
-        self.tf_session.run(output_tensor, {input_tensor, [random.randint(0,5) for i in range(100)]})
+        import numpy as np
+        result = None
+        for i in range(1000):
+            result = self.tf_session.run(output_tensor, {input_tensor: [np.random.uniform(0.0, 10.0, 3)]})
+        return result
