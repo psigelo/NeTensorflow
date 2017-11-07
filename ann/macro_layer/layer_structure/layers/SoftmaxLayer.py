bugfix now Softmax layer have a input amount to solve generic procedures

ann/macro_layer/layer_structure/layers/SoftmaxLayer.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 class SoftmaxLayer(object):
     def __init__(self):
         self.output = None
+        self.input_amount = None
 
     def get_tensor(self):
         if self.output is not None:
@@ -11,12 +12,14 @@ class SoftmaxLayer(object):
         else:
             raise (ValueError, "Softmax Layer not connected, output does not exists")
 
-    @staticmethod
-    def get_input_amount():
-        return NotImplemented
+    def get_input_amount(self):
+        if self.input_amount is None:
+            raise Exception('You must connect this layer before ask input amount because is the same as the previous')
+        return self.input_amount
 
-    def connect_layer(self, _, input_tensor):
+    def connect_layer(self, input_amount, input_tensor):
         self.output = tf.nn.softmax(input_tensor)
+        self.input_amount = input_amount
 
     @staticmethod
     def get_variables():