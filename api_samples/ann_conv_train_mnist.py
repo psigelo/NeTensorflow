aa1396f79623c1e6679b446fd1ddf816bf8180d7
Pascal Sigel <pascal.sigel@gmail.com>
2017-11-15 12:13:14 -0300

    ann_conv_train_mnist.py added:: api sample for convolutional ann usage

---
+++ api_samples/ann_conv_train_mnist.py
@@ -0,0 +1,64 @@
+import tensorflow as tf
+import numpy as np
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+from ann.ANN import ANN
+from ann.macro_layer.MacroLayer import MacroLayer
+from ann.macro_layer.layer_structure.InputLayerStructure import InputLayerStructure
+from ann.macro_layer.layer_structure.LayerStructure import LayerStructure, LayerType
+from ann.macro_layer.layer_structure.layers.ConvolutionalLayer import ConvolutionalLayer
+from ann.macro_layer.layer_structure.layers.FullConnected import FullConnected
+from ann.macro_layer.layer_structure.layers.FullConnectedWithSoftmaxLayer import FullConnectedWithSoftmaxLayer
+from ann.macro_layer.trainers.DefaultTrainer import DefaultTrainer
+
+'''
+    Convolutional neural net, mnist training simple sample
+'''
+
+
+def main():
+    # Input / Output
+    mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+    # Tensorflow
+    tf_sess = tf.Session()
+
+    # Layers
+    input_dim = [None, 28, 28, 1]
+    dataset_dimension = [None, 784]
+    convolutional_layer = ConvolutionalLayer(height_patch=5, width_patch=5, filters_amount=32, strides=[1,1,1,1])
+    logic_layer = FullConnected(inputs_amount=300)
+    out_layer = FullConnectedWithSoftmaxLayer(inputs_amount=10)
+
+    # Layer Structures
+    input_layer_structure = InputLayerStructure(input_dim, dataset_dimension)
+    features_layer_structure = LayerStructure('Features', position=0,
+                                              layer_type=LayerType.IMAGE, layers=[convolutional_layer])
+    logic_layer_structure = LayerStructure('Logic', position=1,
+                                           layer_type=LayerType.ONE_DIMENSION, layers=[logic_layer])
+    output_layer_structure = LayerStructure('Output', position=2,
+                                            layer_type=LayerType.ONE_DIMENSION, layers=[out_layer])
+
+    # Macro Layer
+    macro_layers = MacroLayer(layers_structure=[input_layer_structure, features_layer_structure,
+                                                logic_layer_structure, output_layer_structure])
+
+    # Train
+    trainer = DefaultTrainer(layers_structures=[input_layer_structure, features_layer_structure,
+                                                logic_layer_structure, output_layer_structure])
+
+    # ANN
+    ann = ANN(macro_layers=macro_layers, tf_session=tf_sess, base_folder='./tensorboard_logs/', trainer_list=[trainer])
+    ann.connect_and_initialize()
+
+    # Execute
+    for it in range(1000):
+        batch = mnist.train.next_batch(100)
+        ann.train_step(input_tensor_value=batch[0], output_desired=batch[1].astype(np.float32), global_iteration=it)
+        print("Train iteration: ", it)
+        # print("Time taken in iteration: ", )
+
+
+if __name__ == '__main__':
+    main()
