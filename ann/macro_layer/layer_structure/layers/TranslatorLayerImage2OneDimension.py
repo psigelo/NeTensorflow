fd5cdd00ae871a08e2ee476b7d7d4eb10abf8d25
Pascal Sigel <pascal.sigel@gmail.com>
2017-11-23 14:43:25 -0300

    all variables are properties in layer TranslatorLayerImage2OneDimension

--- ann/macro_layer/layer_structure/layers/TranslatorLayerImage2OneDimension.py
+++ ann/macro_layer/layer_structure/layers/TranslatorLayerImage2OneDimension.py
@@ -1,14 +1,15 @@
 import tensorflow as tf
 
-from ann.macro_layer.layer_structure.LayerStructure import LayerType
+from netensorflow.ann.macro_layer.layer_structure.LayerStructure import LayerType
 
 
 class TranslatorLayerImage2OneDimesion(object):
     def __init__(self):
-        self.inputs_amount = None
-        self.output = None
-        self.layer_structure_name = None
-        self.summaries = list()
+        self.save_and_restore_dictionary = dict()
+        self.__inputs_amount = None
+        self.__output = None
+        self.__layer_structure_name = None
+        self.__summaries = list()
 
     def get_tensor(self):
         if self.output is not None:
@@ -27,3 +28,39 @@ class TranslatorLayerImage2OneDimesion(object):
     @property
     def layer_variables(self):
         return list()
+
+    @property
+    def output(self):
+        return self.__output
+
+    @output.setter
+    def output(self, output):
+        self.__output = output
+        self.save_and_restore_dictionary['output'] = self.__output
+
+    @property
+    def inputs_amount(self):
+        return self.__inputs_amount
+
+    @inputs_amount.setter
+    def inputs_amount(self, inputs_amount):
+        self.__inputs_amount = inputs_amount
+        self.save_and_restore_dictionary['inputs_amount'] = self.__inputs_amount
+
+    @property
+    def summaries(self):
+        return self.__summaries
+
+    @summaries.setter
+    def summaries(self, summaries):
+        self.__summaries = summaries
+        self.save_and_restore_dictionary['summaries'] = self.__summaries
+
+    @property
+    def layer_structure_name(self):
+        return self.__layer_structure_name
+
+    @layer_structure_name.setter
+    def layer_structure_name(self, layer_structure_name):
+        self.__layer_structure_name = layer_structure_name
+        self.save_and_restore_dictionary['layer_structure_name'] = self.__layer_structure_name
