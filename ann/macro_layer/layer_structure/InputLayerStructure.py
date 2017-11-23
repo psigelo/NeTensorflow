d8756085a7b80e53761a5bc9f6be4c8cfa578565
Pascal Sigel <pascal.sigel@gmail.com>
2017-11-23 14:44:13 -0300

    all variables are properties in InputLayerStructure

--- ann/macro_layer/layer_structure/InputLayerStructure.py
+++ ann/macro_layer/layer_structure/InputLayerStructure.py
@@ -1,10 +1,12 @@
-from ann.macro_layer.layer_structure.LayerStructure import LayerStructure, LayerType
 from ann.macro_layer.layer_structure.layers.InputLayer import InputLayer
+
+from ann.macro_layer.layer_structure.LayerStructure import LayerStructure, LayerType
 
 
 class InputLayerStructure(LayerStructure):
     def __init__(self, input_dimension, dataset_dimension=None, layer_structure_name="InputLayer"):
-        self.layer_type = None
+        self.save_and_restore_dictionary = dict()
+        self.__layer_type = None
         if len(input_dimension) == 4:
             self.layer_type = LayerType.IMAGE
         elif len(input_dimension) == 2:
@@ -13,10 +15,18 @@ class InputLayerStructure(LayerStructure):
             raise Exception('LayerType can not be deduced')
         super(InputLayerStructure, self).__init__(layer_structure_name=layer_structure_name, position=0,
                                                   layer_type=self.layer_type)
-        self.__precedence_key = -1  # only for Input Layer
         self.layers = [InputLayer(input_dimension, dataset_dimension)]
 
-        #check all layers have same layertype
+        # check all layers have same layer type
         for layer in self.layers:
             if layer.layer_type != self.layer_type:
                 raise Exception('LayerType is not correctly setted')
+
+    @property
+    def layer_type(self):
+        return self.__layer_type
+
+    @layer_type.setter
+    def layer_type(self, layer_type):
+        self.__layer_type = layer_type
+        self.save_and_restore_dictionary['layer_type'] = self.__layer_type
