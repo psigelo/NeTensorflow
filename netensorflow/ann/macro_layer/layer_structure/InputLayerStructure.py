62ef1664879c5a33f1e5daf94dc685979d4b9246
Pascal Sigel <pascal.sigel@gmail.com>
2017-11-25 03:48:14 -0300

    InputLayerStructure implememnts save_netensorflow_model chain

--- netensorflow/ann/macro_layer/layer_structure/InputLayerStructure.py
+++ netensorflow/ann/macro_layer/layer_structure/InputLayerStructure.py
@@ -1,3 +1,9 @@
+import os
+
+import json
+
+import uuid
+
 from netensorflow.ann.macro_layer.layer_structure.layers.InputLayer import InputLayer
 
 from netensorflow.ann.macro_layer.layer_structure.LayerStructure import LayerStructure, LayerType
@@ -5,6 +11,7 @@ from netensorflow.ann.macro_layer.layer_structure.LayerStructure import LayerStr
 
 class InputLayerStructure(LayerStructure):
     def __init__(self, input_dimension, dataset_dimension=None, layer_structure_name="InputLayer"):
+        self.name = self.__class__.__name__ + '_uuid_' + uuid.uuid4().hex
         self.save_and_restore_dictionary = dict()
         self.__layer_type = None
         if len(input_dimension) == 4:
@@ -30,3 +37,17 @@ class InputLayerStructure(LayerStructure):
     def layer_type(self, layer_type):
         self.__layer_type = layer_type
         self.save_and_restore_dictionary['layer_type'] = self.__layer_type
+
+    def save_netensorflow_model(self, path):
+        layer_structure_path = os.path.join(path, self.name)
+        if not os.path.exists(layer_structure_path):
+            os.mkdir(layer_structure_path)
+
+        store_dict = dict()
+        store_dict['layers'] = [(ls.name, ls.__class__.__name__) for ls in self.layers]
+
+        with open(layer_structure_path + 'data.json', 'w') as fp:
+            json.dump(store_dict, fp)
+
+        for layer in self.layers:
+            layer.save_netensorflow_model(layer_structure_path)
