43804c9ad023062398c10f1fa1801e0b012673e4
Pascal Sigel <pascal.sigel@gmail.com>
2017-11-27 11:15:00 -0300

    ConvolutionalLayer :: restore and save working

--- netensorflow/ann/macro_layer/layer_structure/layers/ConvolutionalLayer.py
+++ netensorflow/ann/macro_layer/layer_structure/layers/ConvolutionalLayer.py
@@ -5,13 +5,14 @@ import tensorflow as tf
 import uuid
 
 from netensorflow.ann.ANNGlobals import register_netensorflow_class
-from netensorflow.ann.macro_layer.layer_structure.LayerStructure import LayerTypeToString
+from netensorflow.ann.macro_layer.layer_structure.LayerStructure import LayerTypeToString, StringToLayerType
 from netensorflow.ann.tensorflow_tools.variable_summaries import variable_summaries
 
 
 @register_netensorflow_class
 class ConvolutionalLayer(object):
-    def __init__(self, height_patch, width_patch, filters_amount, strides, padding='SAME'):
+    def __init__(self, height_patch=None, width_patch=None, filters_amount=None, strides=None,
+                 padding='SAME', restore=False):
         self.name = self.__class__.__name__ + '_uuid_' + uuid.uuid4().hex
         self.save_and_restore_dictionary = dict()
         self.__prev_layer_inputs_amount = None  # must be a list of len 4 [N, H, W, C]
@@ -29,11 +30,12 @@ class ConvolutionalLayer(object):
         self.__width_image = None
         self.__layer_structure_name = None
         self.__summaries = list()
-        self.strides = strides
-        self.padding = padding
-        self.height_patch = height_patch
-        self.width_patch = width_patch
-        self.filters_amount = filters_amount
+        if not restore:
+            self.strides = strides
+            self.padding = padding
+            self.height_patch = height_patch
+            self.width_patch = width_patch
+            self.filters_amount = filters_amount
 
     def get_tensor(self):
         if self.output is not None:
@@ -66,7 +68,7 @@ class ConvolutionalLayer(object):
 
     def save_netensorflow_model(self, path):
         layer_path = os.path.join(path, self.name)
-        with open(layer_path + '_data.json', 'w') as fp:
+        with open(layer_path + '_internal_data.json', 'w') as fp:
             json.dump(self.save_and_restore_dictionary, fp)
 
     @property
@@ -221,3 +223,15 @@ class ConvolutionalLayer(object):
             summaries_ = summaries
         self.__summaries = summaries_
         self.save_and_restore_dictionary['summaries'] = [summary.name for summary in self.__summaries]
+
+    @classmethod
+    def restore_netensorflow_model(cls, path, name):
+        layer_path = os.path.join(path, name)
+        with open(layer_path + '_internal_data.json', 'r') as fp:
+            restore_json_dict = json.load(fp)
+
+        layer = cls(restore=True)
+        for var_name in restore_json_dict:
+            setattr(layer, var_name, restore_json_dict[var_name])
+        layer.name = name
+        return layer
