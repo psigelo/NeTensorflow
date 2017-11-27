ecbfde8def0dd2163b25dca1f30b54bed24bd8f1
Pascal Sigel <pascal.sigel@gmail.com>
2017-11-27 11:09:21 -0300

    letting constructor with restore mode

--- netensorflow/ann/macro_layer/layer_structure/InputLayerStructure.py
+++ netensorflow/ann/macro_layer/layer_structure/InputLayerStructure.py
@@ -12,32 +12,31 @@ from netensorflow.ann.macro_layer.layer_structure.LayerStructure import LayerStr
 
 @register_netensorflow_class
 class InputLayerStructure(LayerStructure):
-    def __init__(self, input_dimension, dataset_dimension=None, layer_structure_name="InputLayer"):
-        self.name = self.__class__.__name__ + '_uuid_' + uuid.uuid4().hex
-        self.save_and_restore_dictionary = dict()
+    def __init__(self, input_dimension=None, dataset_dimension=None, layer_structure_name="InputLayer", restore=False):
+        # Todo: re-ordering this.
         self.__layer_type = None
-        if len(input_dimension) == 4:
-            self.layer_type = LayerType.IMAGE
-        elif len(input_dimension) == 2:
-            self.layer_type = LayerType.ONE_DIMENSION
+        self.save_and_restore_dictionary = dict()
+        if not restore:
+            if len(input_dimension) == 4:
+                layer_type = LayerType.IMAGE
+            elif len(input_dimension) == 2:
+                layer_type = LayerType.ONE_DIMENSION
+            else:
+                raise Exception('LayerType can not be deduced')
+            super(InputLayerStructure, self).__init__(layer_structure_name=layer_structure_name,
+                                                      layer_type=layer_type)
         else:
-            raise Exception('LayerType can not be deduced')
-        super(InputLayerStructure, self).__init__(layer_structure_name=layer_structure_name, layer_type=self.layer_type)
-        self.layers = [InputLayer(input_dimension, dataset_dimension)]
+            super(InputLayerStructure, self).__init__(restore=True)
+        self.name = self.__class__.__name__ + '_uuid_' + uuid.uuid4().hex
+        if not restore:
+            self.layers = [InputLayer(input_dimension, dataset_dimension)]
 
-        # check all layers have same layer type
-        for layer in self.layers:
-            if layer.layer_type != self.layer_type:
-                raise Exception('LayerType is not correctly setted')
-
-    @property
-    def layer_type(self):
-        return self.__layer_type
-
-    @layer_type.setter
-    def layer_type(self, layer_type):
-        self.__layer_type = layer_type
-        self.save_and_restore_dictionary['layer_type'] = self.__layer_type
+            # check all layers have same layer type
+            for layer in self.layers:
+                if layer.layer_type != self.layer_type:
+                    raise Exception('LayerType is not correctly setted')
+        else:
+            super(InputLayerStructure, self).__init__(restore=True)
 
     def save_netensorflow_model(self, path):
         layer_structure_path = os.path.join(path, self.name)
