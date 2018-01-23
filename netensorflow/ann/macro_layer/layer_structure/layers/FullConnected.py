0f8e9a865ef70169104b941e4952ef89bd7565f5
Pascal Sigel <pascal.sigel@gmail.com>
2018-01-22 22:27:33 -0300

    Revert "variables not on cpu"
    This reverts commit d1f9e6e8d6e89ee85e99a64bd91ff3d856278443.

--- netensorflow/ann/macro_layer/layer_structure/layers/FullConnected.py
+++ netensorflow/ann/macro_layer/layer_structure/layers/FullConnected.py
@@ -34,11 +34,13 @@ class FullConnected(AbstractLayer):
     def connect_layer(self, prev_layer, input_tensor):
         with tf.name_scope('FullConnectedVariables'):
             with tf.name_scope('weights'):
-                self.weights = tf.Variable(tf.truncated_normal(
-                    [prev_layer.inputs_amount, self.inputs_amount], stddev=0.1))
+                with tf.device('/cpu:0'):
+                    self.weights = tf.Variable(tf.truncated_normal(
+                        [prev_layer.inputs_amount, self.inputs_amount], stddev=0.1))
                 self.summaries = self.summaries + variable_summaries(self. weights)
             with tf.name_scope('bias'):
-                self.bias = tf.Variable(tf.constant(0.1, shape=[self.inputs_amount]))
+                with tf.device('/cpu:0'):
+                    self.bias = tf.Variable(tf.constant(0.1, shape=[self.inputs_amount]))
                 self.summaries = self.summaries + variable_summaries(self.bias)
             self.output = tf.matmul(input_tensor, self.weights) + self.bias
 
